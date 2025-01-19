4862813f658d3f9813d1183ac973ea09022a5e10
aida20052015 <adjaaidandiayefall@gmail.com>
2025-01-19 22:41:05 +0100

    first commit

--- src/Controller/AidaController.php
+++ src/Controller/AidaController.php
@@ -57,4 +57,60 @@ class AidaController extends AbstractController
     {
         return $this->render('more/more.html.twig');
     }  
+
+    #[Route('/confidentialites', name: 'app_confidentialites')]
+    public function confidentialites(): Response
+    {
+        return $this->render('confidentialites/confidentialites.html.twig');
+    }  
+
+    #[Route('/conditions', name: 'app_conditions')]
+    public function conditions(): Response
+    {
+        return $this->render('conditions/conditions.html.twig');
+    }  
+}
+    // src/Controller/CVController.php
+namespace App\Controller;
+
+use Dompdf\Dompdf;
+use Dompdf\Options;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CVController extends AbstractController
+{
+    
+     #[Route('/cv/downloadCV', name: 'downloadCV')]
+     
+    public function downloadCV(): Response
+    {
+        // Créer une instance de Dompdf
+        $options = new Options();
+        $options->set('isHtml5ParserEnabled', true);
+        $options->set('isPhpEnabled', true);
+        $dompdf = new Dompdf($options);
+
+        // Récupérer le contenu HTML de la page de ton CV
+        $html = $this->renderView('awa/awa.html.twig'); // Assure-toi que le chemin est correct
+
+        // Charger le HTML dans Dompdf
+        $dompdf->loadHtml($html);
+
+        // Définir la taille du papier et l'orientation (A4 en portrait)
+        $dompdf->setPaper('A4', 'portrait');
+
+        // Rendre le PDF
+        $dompdf->render();
+
+        // Générer le PDF dans le flux de réponse HTTP
+        $pdfContent = $dompdf->output();
+
+        // Retourner la réponse avec le PDF (téléchargement)
+        return new Response($pdfContent, 200, [
+            'Content-Type' => 'application/pdf',
+            'Content-Disposition' => 'attachment; filename="cv.pdf"', // Le fichier PDF se télécharge
+        ]);
+    }
 }
